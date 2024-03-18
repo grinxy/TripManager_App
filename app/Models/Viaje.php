d570b720812af2ff5c8a08a11485fd614d1cc82b
Monica <155568667+grinxy@users.noreply.github.com>
2024-03-18 13:41:35 +0100

    rutas viajeController con resource

--- app/Models/Viaje.php
+++ app/Models/Viaje.php
@@ -7,5 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Viaje extends Model
 {
-    use HasFactory;
+    public function reserva()
+    {
+        return $this->belongsTo(Reserva::class, 'id_viaje', 'id');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
-
+use App\Http\Controllers\ReservaController;
+use App\Http\Controllers\ViajeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::resource('/viajes', ViajeController::class);
